8d071f85fe47d31130a3a2f1ca563c15824318fe
teohrnt <teofilushendry5@gmail.com>
2024-07-25 14:08:17 +0700

    feat(personal): update dashboard time 34H

--- components/digital-clock.tsx
+++ components/digital-clock.tsx
@@ -3,18 +3,28 @@
 import React, { useState, useEffect } from 'react';
 
 export const DigitalClock: React.FC = () => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [time, setTime] = useState(new Date().toLocaleTimeString('en-US', {
+        hour12: false, // Use 24-hour format
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }));
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString('en-US', {
+                hour12: false, // Use 24-hour format
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            }));
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
 
-  return (
-    <div className="text-2xl font-bold">
-      <h1>{time}</h1>
-    </div>
-  );
+    return (
+        <div className="text-2xl font-bold">
+            <h1>{time}</h1>
+        </div>
+    );
 }
